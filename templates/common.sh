df923b42d54094fbc183de22a102a61e5233db08
Alexandre Chapellon <alexandre.chapellon@hyland.com>
2024-03-14 00:34:31 +0100

    take it easy in case there's just one context

--- templates/common.sh
+++ templates/common.sh
@@ -2,7 +2,19 @@
 
 checkKontext() {
 	local ACTION_MSG=${1:-"You're about to apply modifications"}
-	KLUSTER=$(kubectl config get-contexts --no-headers=true | awk '/^\*/{print $2}')
-	read -n 1 -p "$ACTION_MSG on Kubernetes cluster $KLUSTER. Is that really what you want to do? (y/n): "
-	[ $REPLY != "y" ] && exit 7
+	HAS_ONE_CONTEXT_ONLY=$(kubectl config get-contexts --no-headers=true | wc -l)
+	case $HAS_ONE_CONTEXT_ONLY in
+		0)
+			echo "No Kubernetes context is set. I can't proceed"
+			exit 7
+			;;
+		1)
+			return 0
+			;;
+		*)
+			KLUSTER=$(kubectl config get-contexts --no-headers=true | awk '/^\*/{print $2}')
+			read -n 1 -p "$ACTION_MSG on Kubernetes cluster $KLUSTER. Is that really what you want to do? (y/n): "
+			[ $REPLY != "y" ] && exit 7
+			;;
+	esac
 }
